5264198b3e06650568be8d6d2ba4f8e1ab53d817
Qingdong Gong <gongqingdonggqd@gmail.com>
2026-07-22 10:28:36 -0700

    Change font to Google Inter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,22 @@
 
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
 import { store } from "./app/store";
 import AppRoutes from "./routes/AppRoutes";
+
+// Same font stack as body in index.css, so Ant Design components match plain text.
+const FONT_FAMILY =
+  '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <ConfigProvider theme={{ token: { fontFamily: FONT_FAMILY } }}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ConfigProvider>
     </Provider>
   );
 }
